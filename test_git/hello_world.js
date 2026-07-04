e9e0915f131f4694daf09873234cffa6c0f151a7
Mbiki-KK <mbiki@DESKTOP-IKD3MK0.localdomain>
2026-07-04 10:45:34 +0300

    relearning node day 2

---
+++ test_git/hello_world.js
@@ -0,0 +1,5 @@
+console.log("This is a simple JavaScript program.");
+const path = require('path');
+const filePath = path.join(__dirname, 'hello_world.js');
+
+console.log(`The file path is: ${filePath}`); 
