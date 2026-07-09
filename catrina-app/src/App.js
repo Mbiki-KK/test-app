c3aa009eaa3426274306be14d5dd6fc50dc6455b
Mbiki-KK <mbiki@DESKTOP-IKD3MK0.localdomain>
2026-07-09 11:40:00 +0300

    Initial commit

---
+++ catrina-app/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import styles from 'styled-components';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
