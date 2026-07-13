d475a47519752d866f75545d9fccd78708013aee
Mbiki-KK <mbiki@DESKTOP-IKD3MK0.localdomain>
2026-07-13 12:51:39 +0300

    Styled app.js

--- catrina-app/src/App.js
+++ catrina-app/src/App.js
@@ -1,16 +1,20 @@
 // import logo from './logo.svg';
-// import './App.css';
 import styles from 'styled-components';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  background-color: #a7b7f6;
+  min-height: 100vh;`;
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Header />
       <Home />
 
-    </div>
+    </AppContainer>
   );
 }
 
